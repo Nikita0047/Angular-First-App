00aaeb1f1b4aaa01ccd313906c36d533f7c3d6dd
Nikita Kumari <nikitasuprstr@gmail.com>
2026-01-04 23:09:07 +0530

    making an counter

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,17 +1,30 @@
 import { Component, signal } from '@angular/core';
 import{ Greeting } from '../components/greeting/greeting';
+import{ Counter } from '../components/counter/counter';
 @Component({
   selector: 'app-home',
-  imports: [Greeting],
+  imports: [Greeting,Counter],
   template: `
     <p>
       home works!
     </p>
     <app-greeting [message]="myMessage()"/>
+    <app-counter></app-counter>
     <!-- Event binding example -->
-    <input type="text" (keyup)="keyupHandler($event)"/>
+    <input
+     type="text" 
+     (keyup)="keyupHandler($event)"
+     placeholder="Type here..."/>
   `,
-  styles: ``,
+  styles: `
+  input{
+    border:2px solid #2e4d62ff;
+    margin:10px;
+    padding:5px;
+    font-size:16px;
+    length:500px;
+  }
+  `,
 })
 export class Home {
 myMessage = signal('Welcome to the Home Component');// data passing from home to greeting component(parent to child) using signal
