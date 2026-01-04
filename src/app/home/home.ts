e4f4b5cf80dc453e65a4be7d90b763b8e350554c
Nikita Kumari <nikitasuprstr@gmail.com>
2026-01-04 20:33:45 +0530

    addedEventlistner

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -8,9 +8,16 @@ import{ Greeting } from '../components/greeting/greeting';
       home works!
     </p>
     <app-greeting [message]="myMessage()"/>
+    <!-- Event binding example -->
+    <input type="text" (keyup)="keyupHandler($event)"/>
   `,
   styles: ``,
 })
 export class Home {
 myMessage = signal('Welcome to the Home Component');// data passing from home to greeting component(parent to child) using signal
+//EventListener example
+// method to handle keyup event
+keyupHandler(event:KeyboardEvent){
+  console.log(`user has pressed ${event.key} key.`);
+}
 }
