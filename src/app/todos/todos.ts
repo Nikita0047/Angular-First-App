227b162d0a6c1888d8ed6f0d27df3432f2de7876
Nikita Kumari <nikitasuprstr@gmail.com>
2026-01-06 18:50:09 +0530

    Worked with Http Calls and Json data fetch

--- src/app/todos/todos.ts
+++ src/app/todos/todos.ts
@@ -1,6 +1,7 @@
 import { Component, inject,OnInit, signal ,} from '@angular/core';
 import{ Todoservice } from '../services/todos';
 import { Todo } from '../models/todos.type';
+import { catchError } from 'rxjs';
 @Component({
   selector: 'app-todos',
   imports: [],
@@ -10,19 +11,50 @@ import { Todo } from '../models/todos.type';
     </p>
     <p>
       {{ TodoList()[0].title }}  displaying elements from TodoList -->
-    @for(todo of TodoList(); track todo.id){
-    <p>
-      {{todo.title}}
-    </p>
-    }
+
+      <h3>Todo List</h3>
+      <ul class="todo">
+        @for(todo of TodoList(); track todo.id){
+          <li class="todo__item">
+            <input id="todo-{{todo.id}}" type="checkbox" [value]="todo.completed"/>
+              <label for="todo-{{todo.id}}">
+              {{todo.title}}
+              </label> 
+          </li>
+       }
+      </ul>
+   
+    
   `,
-  styles: ``,
+  styles:
+   `
+    .todo{
+      __item{
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        
+      }
+    }`,
 })
 export class Todos implements OnInit {
 todoService = inject(Todoservice);
 TodoList = signal<Array<Todo>>([]);
 ngOnInit():void{
-  console.log(this.todoService.TodoItems);
-  this.TodoList.set(this.todoService.TodoItems);
-}
+  //console.log(this.todoService.TodoItems);
+  //this.TodoList.set(this.todoService.TodoItems);
+  this.todoService.getTodosfromAPI()
+  .pipe(
+    catchError((err)=>{
+      console.log( err);
+
+      throw err;
+    }))
+    .subscribe
+      ( (todo) => 
+        {
+        this.TodoList.set(todo);
+        }
+      );
+    }
 }
